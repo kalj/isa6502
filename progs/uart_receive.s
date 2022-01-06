3257f248773b6e7e5b03b66c5eec9407f10cb22d
Karl Ljungkvist <k.ljungkvist@gmail.com>
2022-01-06 20:47:07 +0100

    improve functionality of CR, and add BS/DEL support

--- progs/uart_receive.s
+++ progs/uart_receive.s
@@ -40,29 +40,99 @@ receive_loop:
     ;; There is at least one received byte to read
     LDA UART_REG_RXDATA
 
-    CMP #$0d                    ; CR
-    BNE printable_test
+    ;; BS/DEL received?
+    CMP #$08
+    BEQ bsdel_case
+    CMP #$7f
+    BEQ bsdel_case
 
-cr_received:
-    ;; switch line
-    LDA DPY_REG_CMD             ; Load CMD register
-    CLC
-    ADC #%01000000              ; toggle msb of addr
-    ORA #%10000000              ; set msb of to convert to command
-    JSR lcd_send_cmd
-    BRA receive_loop
+    ;; CR received?
+    CMP #$0d
+    BEQ cr_case
 
-printable_test:
+    ;; test for printable
     CMP #$20                    ; space
     BCC receive_loop            ; < space
     CMP #$7e                    ; ~
     BCS receive_loop            ; >= ~
+    BRA printable_case
 
-    ;; print it
-char_received:
+bsdel_case:
+    ;; determine if on beginning of line
+    LDA DPY_REG_CMD             ; Load CMD register
+    BIT #%00111111
+    BEQ receive_loop            ; beginning of line -> done
+
+    ;; Decrement address
+    LDA #%00010000
+    JSR lcd_send_cmd
+
+    ;; Write space
+    LDA #$20
+    JSR lcd_send_byte
+
+    ;; Decrement address
+    LDA #%00010000
+    JSR lcd_send_cmd
+
+    BRA receive_loop            ; done
+
+cr_case:
+    ;; Determine if first line or not
+    LDA DPY_REG_CMD             ; Load CMD register
+    AND #%01000000
+    BEQ not_on_line2
+
+    ;; copy line2 to line1, clear line2, and go to beginning of second line
+
+    ;; First read line2, read happens backward since DDRAM addr is decreased when reading
+
+    ;; start at position 15, second line
+    LDA #%11000000
+    JSR lcd_send_cmd
+    LDX #16                      ; start loop index at 16, decrement until 0
+
+read_loop:
+    JSR lcd_read_byte           ; increments address
+    STA 0,X                     ; store read value in zero-page with x as index
+    DEX                         ; decrement X, setting Z flag if 0 is hit
+    BNE read_loop               ; loop if not 0
+
+    ;; Now pop from stack and write to row 1, now forwards
+
+    ;; start at position 0, first line
+    LDA #%10000000
+    JSR lcd_send_cmd
+    LDX #16                      ; start loop index at 16, decrement until 0
+write_loop:
+    LDA 0,X                      ; read value from zero-page with x as index
+    JSR lcd_send_byte            ; write and increment address
+    DEX                          ; decrement X, setting Z flag if 0 is hit
+    BNE write_loop               ; loop if not 0
+
+    ;; Last clear out second line
+    ;; start at position 0, second line
+    LDA #%11000000
+    JSR lcd_send_cmd
+    LDX #16                      ; start loop index at 16, decrement until 0
+    LDA #$20                     ; load a space char
+
+clear_loop:
+    JSR lcd_send_byte            ; write and increment address
+    DEX                          ; decrement X, setting Z flag if 0 is hit
+    BNE clear_loop               ; loop if not 0
+    ;; Fall through to other case to get back to beginning of second line
+
+not_on_line2:
+    ;; go to beginning of second line
+    LDA #%11000000
+    JSR lcd_send_cmd
+    BRA receive_loop            ; done
+
+printable_case:
     JSR lcd_send_byte
     ;; restart
-    BRA receive_loop
+    BRA receive_loop            ; done
 
     STP
     NOP
@@ -82,4 +152,9 @@ lcd_send_cmd:
 lcd_send_byte:
     JSR lcd_wait
     STA DPY_REG_DATA            ; Write accumulator to DATA register
+    RTS                         ; Return
+
+lcd_read_byte:
+    JSR lcd_wait
+    LDA DPY_REG_DATA            ; Read into accumulator from DATA register
     RTS                         ; Return
